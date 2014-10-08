f27a90310c7cb304f45c6452b0ba861de3485db0
sofia ma <sofia.ma@gmail.com>
2014-10-08 14:30:47 -0400

    completed spotify char

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,21 +1,24 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
 
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
-    # return a string that is the base url + / + preference + / + region + / + latest
+    # return a string that is the base url + / + preference + / + region + / + latest 
+    @base_url + preference + '/' + region + '/latest'
   end
 
   def get_json(url)
     # load json given a url here
     # refer to the references if you have questions about this
+    JSON.load(open(url))
   end
 
   def fetch_track_album_artist(music_hash)
@@ -42,27 +45,33 @@ class SpotifyChart
   
     # the track name, artist name, and album name should be the first track in the
     # tracks array
+
+    first_track = music_hash["tracks"].first
+    "'#{first_track["track_name"]}' by #{first_track["artist_name"]} from the album #{first_track["album_name"]}"
   end
 
 
   def most_streamed(region)
     # call no get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
-
+    new_url = get_url('most_streamed',region)
     # call on get_json here, using the string that get_url returns
-    
+    get_json(new_url)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+    #binding.pry
+    fetch_track_album_artist(get_json(new_url))
   end
 
   def most_shared(region)
     # call no get_url here, where preference is the string 'most_shared',
     # and set it equal to a variable
-
+    new_url = get_url('most_shared',region)
     # call on get_json here, using the string that get_url returns
-    
+    get_json(new_url)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+    fetch_track_album_artist(get_json(new_url))
   end
 
 end
